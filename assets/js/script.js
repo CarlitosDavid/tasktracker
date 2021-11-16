4768930933c5a138377b52573efa2055d5f74740
Carl Contreras <contrerascarl09@gmail.com>
2021-11-16 13:52:25 -0600

    added .js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+console.dir(window.document);
+/* buttom notes are for console log on web browser */
+/* window.document.querySelector("button"); */
+/* var btn = window.document.querySelector("button");
+console.dir(btn); */
